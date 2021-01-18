1066d3b91b31d94338bb30ddc693d16e305d7014
EgorAlekseev <54812985+EgorAlekseev@users.noreply.github.com>
2021-01-18 15:06:08 +0300

    Add files

---
+++ Labs/Project 1_2B/Project 1_2B/Project_1_2B.py
@@ -0,0 +1,7 @@
+#You will create three lists that each contain ten values.  One will contain product names, another product 
+#costs and the third a three-digit employee code.  Your task will be to merge all three lists into a single navigable array
+
+import module
+task = input("Введите id продукта")
+print("Наименование продукта: " + module.names[module.code.index(task)])
+print("Стоимость продукта: " + str(module.costs[module.code.index(task)]))
---
+++ Labs/Project 1_2B/Project 1_2B/module.py
@@ -0,0 +1,4 @@
+names = ["product_1", "product_2", "product_3", "product_4", "product_5", "product_6", "product_7", \
+    "product_8", "product_9", "product_10"]
+costs = [25, 14, 16, 13, 87, 65, 45, 29, 64, 10]
+code = ["001", "002", "003", "004", "005", "006", "007", "008", "009", "010"]
